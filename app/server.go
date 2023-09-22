85fd9d04b7001c145769d264ea547c2a3737c390
Bekian <Bekian.Cat@gmail.com>
2023-09-21 19:58:47 -0500

    stage 7

--- app/server.go
+++ app/server.go
@@ -1,44 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
+
 	"path"
 	"strings"
+	// Uncomment this block to pass the first stage
+	// "net"
+	// "os"
 )
 
 func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+	fmt.Printf("Logs from your program will appear here! %v, %v\n", len(os.Args), os.Args)
 
-	fmt.Printf("Logs appear here: %v, %v\n", len(os.Args), os.Args)
 	fmt.Println(os.Args)
+	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		handleError(err)
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
 	}
-	defer l.Close()
-	//fmt.Println("l: ", l)
 	for {
 		connection, err := l.Accept()
 		if err != nil {
-			handleError(err)
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
 		go handleConnection(err, connection)
 	}
 }
-
 func handleConnection(err error, connection net.Conn) {
 	if err != nil {
-		handleError(err)
+		handleErr(err)
 	}
 	// process request
 	request := make([]byte, 1024)
 	_, err = connection.Read(request)
 	if err != nil {
-		handleError(err)
+		handleErr(err)
 	}
 	requestData := string(request)
 	fmt.Printf("Data: \n%s", requestData)
@@ -46,9 +50,7 @@ func handleConnection(err error, connection net.Conn) {
 	for i, datum := range httpData {
 		fmt.Printf("i=%v -- Data: %v----\n", i, datum)
 	}
-
 	startLine := parseStartLine(httpData[0])
-
 	if startLine.Path == "/" {
 		connection.Write([]byte(("HTTP/1.1 200 OK\r\n\r\n")))
 	} else if strings.HasPrefix(startLine.Path, "/echo/") {
@@ -59,32 +61,30 @@ func handleConnection(err error, connection net.Conn) {
 		connection.Write([]byte((res)))
 	} else if strings.HasPrefix(startLine.Path, "/files/") {
 		var res = handleFiles(os.Args[2], startLine.Path)
+
 		connection.Write([]byte((res)))
 	} else {
 		connection.Write([]byte(("HTTP/1.1 404 NOT FOUND\r\n\r\n")))
 	}
-
 	connection.Close()
 }
-
 func handleFiles(directory string, httpPath string) string {
 	filePath := strings.TrimPrefix(httpPath, "/files/")
 	filePathAbs := path.Join(directory, filePath)
-	//fileContent, err := os.ReadFile(filePathAbs)
-	file, err := os.OpenFile(filePathAbs, os.O_RDONLY, os.ModePerm)
+	fileContent, err := os.ReadFile(filePathAbs)
+	//file, err := os.OpenFile(filePathAbs, os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		fmt.Println("error occurred: ", err.Error())
 		return "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 	}
-	fileBuffer := bufio.NewReader(file)
-	fileSize := fileBuffer.Size()
-	fileContents := make([]byte, fileSize)
-	_, err = io.ReadFull(fileBuffer, fileContents)
-	handleError(err)
-	return make200Response(string(fileContents), "application/octet-stream")
+	//fileBuffer := bufio.NewReader(file)
+	//fileSize := fileBuffer.Size()
+	//fileContent := make([]byte, fileSize)
+	//_, err = io.ReadFull(fileBuffer, fileContent)
+	//handleErr(err)
+	return make200Response(string(fileContent), "application/octet-stream")
 
 }
-
 func userAgent(httpData []string) string {
 	// skip 0 - it is start line
 	for i := 1; i < len(httpData); i++ {
@@ -93,15 +93,11 @@ func userAgent(httpData []string) string {
 		}
 	}
 	return ""
-
 }
-
 func echoResponse(path string) string {
 	content := strings.TrimPrefix(path, "/echo/")
 	return make200Response(content, "text/plain")
-
 }
-
 func make200Response(content string, contentType string) string {
 	response := make([]string, 5)
 	response[0] = "HTTP/1.1 200 OK"
@@ -110,9 +106,7 @@ func make200Response(content string, contentType string) string {
 	response[3] = CONTENT_SEPARATOR
 	response[4] = content
 	return strings.Join(response, LINE_SEPARATOR)
-
 }
-
 func parseStartLine(line string) StartLine {
 	items := strings.Split(line, " ")
 	if len(items) != 3 {
@@ -123,7 +117,6 @@ func parseStartLine(line string) StartLine {
 		Path:        items[1],
 		HttpVersion: items[2],
 	}
-
 }
 
 type StartLine struct {
@@ -138,7 +131,7 @@ const LINE_SEPARATOR = "\r\n"
 //goland:noinspection GoSnakeCaseUsage
 const CONTENT_SEPARATOR = ""
 
-func handleError(err error) {
+func handleErr(err error) {
 	if err != nil {
 		fmt.Println("error occurred: ", err.Error())
 		os.Exit(1)
