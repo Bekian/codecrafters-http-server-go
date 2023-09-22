d8602fcd7e4a76ef85d7fdb06d5b2ea90f7c3093
Bekian <Bekian.Cat@gmail.com>
2023-09-21 19:01:25 -0500

    stage3?

--- app/server.go
+++ app/server.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		handleError(err)
 	}
 	defer l.Close()
 	//fmt.Println("l: ", l)
@@ -19,17 +20,46 @@ func main() {
 		if err != nil {
 			handleError(err)
 		}
-
-		var read []byte
-		_, err = connection.Read(read)
+		request := make([]byte, 1024)
+		_, err = connection.Read(request)
 		if err != nil {
 			handleError(err)
 		}
+		requestData := string(request)
+		fmt.Printf("Data: \n%s", requestData)
+		splitData := strings.Split(requestData, "\r\n")
+		for i, datum := range splitData {
+			fmt.Printf("i=%v -- Data: %v----\n", i, datum)
+		}
+		startLine := parseStartLine(splitData[0])
+		if startLine.Path == "/" {
+			connection.Write([]byte(("HTTP/1.1 200 OK\r\n\r\n")))
+		} else {
+			connection.Write([]byte(("HTTP/1.1 404 NOT FOUND\r\n\r\n")))
 
-		connection.Write([]byte(("GET /index.html HTTP/1.1 200 OK\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\n")))
+		}
 
 		connection.Close()
 	}
+}
+
+func parseStartLine(line string) StartLine {
+	items := strings.Split(line, " ")
+	if len(items) != 3 {
+		log.Fatal("Expect 'HTTP_METHOD<space>PATH<space>HTTP_VERSION'")
+	}
+	return StartLine{
+		HttpMethod:  items[0],
+		Path:        items[1],
+		HttpVersion: items[2],
+	}
+
+}
+
+type StartLine struct {
+	HttpMethod  string
+	Path        string
+	HttpVersion string
 }
 
 func handleError(err error) {
